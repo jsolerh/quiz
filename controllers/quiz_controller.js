3ba12ffcc01e5975b094b368d4e80669ae1394b6
jsolerh <jimmy_092@hotmail.com>
2016-05-25 14:23:28 +0200

    Despliegue en Heroku

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,12 @@
+//GET /question
+exports.question = function(req, res, next) {
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: '¿Capital de Italia?', answer: answer});
+};
+
+//GET /check
+exports.check = function(req, res, next) {
+	var answer = req.query.answer || '';
+	var result = ((answer === 'Roma') ? 'Correcta' : 'Incorrecta');
+	res.render('quizzes/result', {result: result, answer: answer});
+};
